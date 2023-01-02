a899b4a498fb7f9e8f9ed551de61d81e860cf1b2
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-01-02 04:14:18 -0500

    Added styling

--- js/script.js
+++ js/script.js
@@ -15,26 +15,35 @@ const displayController = (() => {
     const resetBtn = document.querySelector('.reset_btn')
 
     const createBoard = () => {
-        for (const [index, element] of gameBoard.game.entries()){
+        for (const element of gameBoard.game){
             const displayBox = document.createElement('div');
+            const imgMarker = document.createElement('img');
+            imgMarker.classList.add('marker_img');
             displayBox.classList.add('display_box');
-            displayBox.classList.add(index);
             displayBox.classList.add('empty_box');
+            displayBox.appendChild(imgMarker)
+            imgMarker.style.display = 'none'
             displayContainer.appendChild(displayBox);
         };
     }
     const addMarker = (e) => {
-        if (e.target.classList[0] === 'display_box' && Player1.turn === true && e.target.classList[2] === 'empty_box'){
-            gameBoard.game.splice(e.target.classList[1],1,Player1.marker);
-            e.target.textContent = Player1.marker;
-            markerX.push(Number(e.target.classList[1]));
+        const parent = e.target.parentElement;
+        const index = Array.prototype.indexOf.call(parent.children, e.target)
+        if (e.target.classList[0] === 'display_box' && Player1.turn === true && e.target.classList[1] === 'empty_box'){
+            gameBoard.game.splice(index,1,Player1.marker);
+            e.target.childNodes[0].src = './imgs/x_icon.png'
+            e.target.childNodes[0].style.display = 'flex'
+            markerX.push(Number(index));
+            e.target.classList.add('x_marker')
             e.target.classList.remove('empty_box');
             Player1.turn = false;
             Player2.turn = true;
-        }else if (e.target.classList[0] === 'display_box' && Player2.turn === true && e.target.classList[2] === 'empty_box'){
-            gameBoard.game.splice(e.target.classList[1],1,Player2.marker)
-            e.target.textContent = Player2.marker;
-            markerO.push(Number(e.target.classList[1]));
+        }else if (e.target.classList[0] === 'display_box' && Player2.turn === true && e.target.classList[1] === 'empty_box'){
+            gameBoard.game.splice(index,1,Player2.marker)
+            e.target.childNodes[0].src = './imgs/heart_icon.png'
+            e.target.childNodes[0].style.display = 'flex'
+            markerO.push(Number(index));
+            e.target.classList.add('o_marker')
             e.target.classList.remove('empty_box');
             Player2.turn = false;
             Player1.turn = true;
@@ -45,9 +54,11 @@ const displayController = (() => {
         gameBoard.game.forEach((element, index) =>{
             gameBoard.game[index] = ''
         })
-        displayContainer.childNodes.forEach((box,index) => {
-            displayContainer.childNodes[index].textContent = ''
+        displayContainer.childNodes.forEach((box) => {
+            box.childNodes[0].style.display = 'none'
             box.classList.add('empty_box');
+            box.classList.remove('x_marker');
+            box.classList.remove('o_marker');
         })
         Player1.turn = true;
         Player2.turn = false;
@@ -83,31 +94,31 @@ const gameFlow = (() => {
     };
 
     const removeClass = () => {
-        for (const child of displayController.displayContainer.children){
-            child.classList.remove('empty_box')
+        for (const child of displayController.displayContainer.childNodes){
+            child.classList.remove('empty_box');
+            
         }
     };
 
     const winChecker = () => {
-       
         for (const win of winningPatterns){
             const winnerX = win.every(elem => displayController.markerX.includes(elem)); 
             const winnerO = win.every(elem => displayController.markerO.includes(elem));
 
             if(winnerX){
-                winnerContainer.textContent = 'Player 1 wins';
+                winnerContainer.textContent = 'Player 1 wins!';
                 removeClass()
-                displayController.resetBtn.style.display = 'block'
+                displayController.resetBtn.style.display = 'flex'
             }
             else if(winnerO){
-                winnerContainer.textContent = 'Player 2 wins'
+                winnerContainer.textContent = 'Player 2 wins!'
                 removeClass()
-                displayController.resetBtn.style.display = 'block'
+                displayController.resetBtn.style.display = 'flex'
             }
-            else if(tieChecker()){
-                winnerContainer.textContent = "It's a tie"
+            else if(tieChecker() && (!winnerX || !winnerO)){
+                winnerContainer.textContent = "It's a tie!"
                 removeClass()
-                displayController.resetBtn.style.display = 'block'
+                displayController.resetBtn.style.display = 'flex'
             }
         }
     }
@@ -117,13 +128,8 @@ const gameFlow = (() => {
     return{winnerContainer}
 })();
 
-const Computer = () => {
-
-}
 
 displayController.createBoard()
-
-
 
 
 
